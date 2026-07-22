e4beebd62f5afbe2ff35a4f2361092c65018e53b
Irana <iranamebratu570@gmail.com>
2026-07-22 10:59:47 +0300

    version 1

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 console.log("Hello, this is the config file");
-console.log(" config version 1");
+console.log(" config version 2");
+console.log("config file updated");
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log(" index.js file");
-console.log(" version 1");
+console.log(" version 2 updated");
